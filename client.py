6d28b2f15624a5a17d802b6adf77f0bac0b19d37
yan-code1 <1977435273@qq.com>
2021-03-22 11:17:13 +0800

    first vision

---
+++ client.py
@@ -0,0 +1,52 @@
+from bluetooth import *
+import sys
+
+if sys.version < '3':
+    input = raw_input
+
+addr = None
+
+if len(sys.argv) < 2:
+    print("no device specified.  Searching all nearby bluetooth devices for")
+    print("the SampleServer service")
+else:
+    addr = sys.argv[1]
+    print("Searching for SampleServer on %s" % addr)
+
+# search for the SampleServer service
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+service_matches = find_service( uuid = uuid, address = addr )
+
+if len(service_matches) == 0:
+    print("couldn't find the SampleServer service =(")
+    sys.exit(0)
+
+first_match = service_matches[0]
+port = first_match["port"]
+name = first_match["name"]
+host = first_match["host"]
+
+print("connecting to \"%s\" on %s" % (name, host))
+
+# Create the client socket
+sock=BluetoothSocket( RFCOMM )
+sock.connect((host, port))
+
+print("connected.  type stuff")
+
+#receive
+try:
+    while True:
+        data = client_sock.recv(1024)
+        if len(data) == 0: break
+        print("received [%s]" % data)
+except IOError:
+    pass
+print("disconnected")
+
+#send
+# while True:
+#     data = input()
+#     if len(data) == 0: break
+#     sock.send(data)
+sock.close()
